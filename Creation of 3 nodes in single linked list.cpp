8029380ff23df5fa351cb1afbf298f088da61a6a
Vriddhi Bhardwaj <68302775+vriddhi1203@users.noreply.github.com>
2020-10-06 23:48:58 +0530

    Update Creation of 3 nodes in single linked list.cpp

--- Creation of 3 nodes in single linked list.cpp
+++ Creation of 3 nodes in single linked list.cpp
@@ -1,4 +1,4 @@
-//......Creation of 3 nodes in a single linked list.................//
+//......Creation of 4 nodes in a single linked list.................//
 
 #include <iostream>
 using namespace std;
@@ -12,16 +12,20 @@ int main(){
 	Node* head = NULL;
 	Node* second = NULL;
 	Node* third = NULL;
+	Node* fourth = NULL;
 
 	head = new Node();
 	second = new Node();
 	third = new Node();
+	fourth = new Node();
 
 	head->data = 1;
 	head->next = second;
 	second->data = 2;
 	second->next = third;
 	third->data = 3;
-	third->next = NULL;
+	third->next = fourth;
+	fourth->data = 4;
+	fourth->next = NULL;
 	return 0;
 }
