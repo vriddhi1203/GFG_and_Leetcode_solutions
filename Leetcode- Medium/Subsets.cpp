53c85f9f838bd1da3e7c87b9bc4985c8204fbc1d
Vriddhi Bhardwaj <68302775+vriddhi1203@users.noreply.github.com>
2021-09-24 15:12:02 +0530

    Add files via upload

---
+++ Leetcode- Medium/Subsets.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<vector<int>> ans;
+    
+    void printAllSubsets(vector<int>& nums,int n,int i,vector<int> temp){
+        if(i==n){
+            ans.push_back(temp);
+            return;
+        }
+        //chose no to put element in the subset
+        printAllSubsets(nums,n,i+1,temp);
+        
+        //chose yes to put element in the subset
+        temp.push_back(nums[i]);
+        printAllSubsets(nums,n,i+1,temp);
+    }
+    
+    vector<vector<int>> subsets(vector<int>& nums) {
+        int n=nums.size();
+        int i=0;
+        vector<int> temp;
+        printAllSubsets(nums,n,i,temp);
+        return ans;
+    }
+};
