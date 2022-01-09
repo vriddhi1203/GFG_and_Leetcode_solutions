6791a6856e6e3583e0bf5f2644ad47f2f4559a62
Vriddhi Bhardwaj <68302775+vriddhi1203@users.noreply.github.com>
2022-01-10 01:34:04 +0530

    Update Remove_Duplicates_from_Sorted_Array.cpp

--- Leetcode- Easy/Remove_Duplicates_from_Sorted_Array.cpp
+++ Leetcode- Easy/Remove_Duplicates_from_Sorted_Array.cpp
@@ -1,3 +1,6 @@
+/* Brute Force Approach - taking a set and then storing the array elements in it and then printing it out but that would take extra space and 
+ space complexity won't be O(1) there */
+
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
@@ -9,6 +12,30 @@ public:
         int i=0;
         for(int j=1;j<n;j++){
             if(nums[i]!=nums[j]){
+                i++;
+                nums[i]=nums[j];
+            }
+        }
+        
+        return i+1;
+    }
+};
+
+// another solution with same approach - 
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int n=nums.size();
+        if(n==0)
+            return 0;
+        if(n==1)
+            return 1;
+        int i=0,j=0;
+        while(j<n){
+            if(nums[i]==nums[j])
+                j++;
+            else{
                 i++;
                 nums[i]=nums[j];
             }
