c4fa2117f4920d1d95e2cdcb5705c321d8c29436
Vriddhi Bhardwaj <68302775+vriddhi1203@users.noreply.github.com>
2022-01-20 19:53:28 +0530

    Update Sqrt(x).cpp

--- Leetcode- Easy/Sqrt(x).cpp
+++ Leetcode- Easy/Sqrt(x).cpp
@@ -1,3 +1,28 @@
+//Using Binary Search approach
+
+class Solution {
+public:
+    int mySqrt(int x) {
+        int l=0,h=x;
+        while(l<=h){
+            long long int mid=(l+h)/2;
+            if(mid*mid>x)
+                h=mid-1;
+            else if(mid*mid==x)
+                return mid;
+            else{
+                if((mid+1)*(mid+1)>x)
+                    return mid;
+                else
+                    l=mid+1;
+            }
+        }
+        return -1;
+    }
+};
+
+//Brute Force approach (using sqrt built in function)
+
 class Solution {
 public:
     int mySqrt(int x) {
