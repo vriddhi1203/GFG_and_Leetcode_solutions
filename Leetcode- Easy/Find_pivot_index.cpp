6c86c38d6654819eff95ec53ea9f15441b58d8c9
Vriddhi Bhardwaj <68302775+vriddhi1203@users.noreply.github.com>
2022-02-11 02:52:03 +0530

    Add files via upload

---
+++ Leetcode- Easy/Find_pivot_index.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> prefixsum;
+        vector<int> suffixsum;
+        int num=0;
+        for(int i=0;i<n;i++){
+            if(i==0)
+                prefixsum.push_back(0);
+            else{
+                num+=nums[i-1];
+                prefixsum.push_back(num);
+            }
+        }
+        num=0;
+        for(int i=n-1;i>=0;i--){
+            if(i==n-1)
+                suffixsum.push_back(0);
+            else{
+                num+=nums[i+1];
+                suffixsum.push_back(num);
+            }
+        }
+        reverse(suffixsum.begin(),suffixsum.end());
+        for(int i=0;i<n;i++){
+            if(prefixsum[i]==suffixsum[i])
+                return i;
+        }
+        return -1;
+    }
+};
