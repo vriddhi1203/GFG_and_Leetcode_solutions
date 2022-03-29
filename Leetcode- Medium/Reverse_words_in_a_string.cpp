2f7077df1fdb2a00276604b12f8d9990546dea8c
Vriddhi Bhardwaj <68302775+vriddhi1203@users.noreply.github.com>
2022-03-30 02:00:52 +0530

    Add files via upload

---
+++ Leetcode- Medium/Reverse_words_in_a_string.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        int n=s.length();
+        string result;
+        int i=0;
+        while(i<n){
+            while(i<n && s[i]==' '){
+                i++;
+            }
+            if(i>=n)
+                break;
+            int j=i+1;
+            while(j<n && s[j]!=' '){
+                j++;
+            }
+            string w=s.substr(i,j-i);
+            if(result.length()==0)
+                result=w;
+            else
+                result=w+" "+result;
+            i=j+1;
+        }
+        return result;
+    }
+};
